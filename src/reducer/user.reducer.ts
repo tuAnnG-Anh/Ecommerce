d6e02672856a01bc665e2d1732074e33ed82fa7b
Tuan Nguyen <anhtuan.ngw@gmail.com>
2023-11-23 18:01:43 +0700

    push

--- src/reducer/user.reducer.ts
+++ src/reducer/user.reducer.ts
@@ -1,31 +1,31 @@
-import { create } from "zustand";
+// import { create } from "zustand";
 
-type State = {
-  count: number;
-};
+// type State = {
+//   count: number;
+// };
 
-type Actions = {
-  increment: (qty: number) => void;
-  decrement: (qty: number) => void;
-};
+// type Actions = {
+//   increment: (qty: number) => void;
+//   decrement: (qty: number) => void;
+// };
 
-type Action = {
-  type: keyof Actions;
-  qty: number;
-};
+// type Action = {
+//   type: keyof Actions;
+//   qty: number;
+// };
 
-const countReducer = (state: State, action: Action) => {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + action.qty };
-    case "decrement":
-      return { count: state.count - action.qty };
-    default:
-      return state;
-  }
-};
+// const countReducer = (state: State, action: Action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { count: state.count + action.qty };
+//     case "decrement":
+//       return { count: state.count - action.qty };
+//     default:
+//       return state;
+//   }
+// };
 
-const useCountStore = create<State & Actions>((set) => ({
-  count: 0,
-  dispatch: (action: Action) => set((state) => countReducer(state, action)),
-}));
+// const useCountStore = create<State & Actions>((set) => ({
+//   count: 0,
+//   dispatch: (action: Action) => set((state) => countReducer(state, action)),
+// }));
